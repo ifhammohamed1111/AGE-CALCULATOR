8b5099fc3472d794ddb834fece124f43b89a361e
Mohamed MRI <mohamedmri.21@uom.lk>
2024-03-03 19:43:28 +0530

    project setuped and finished

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class' or 'false
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
